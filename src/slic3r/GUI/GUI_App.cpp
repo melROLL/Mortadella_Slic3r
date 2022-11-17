99ce8a8d0d3f37844d112cbb0adc8a387244f29d
melROLL <60772441+melROLL@users.noreply.github.com>
2022-11-17 12:05:58 +0100

    contributions moded

--- src/slic3r/GUI/GUI_App.cpp
+++ src/slic3r/GUI/GUI_App.cpp
@@ -282,7 +282,7 @@ private:
             // credits infornation
             credits = _L("SuperSlicer is a skinned version of Slic3r, based on PrusaSlicer by Prusa and the original Slic3r by Alessandro Ranellucci & the RepRap community.") + "\n\n" +
                         title + " " + _L("is licensed under the") + " " + _L("GNU Affero General Public License, version 3") + "\n\n" +
-                        _L("Contributions by Vojtech Bubnik, Enrico Turri, Durand Remi, Oleksandra Iushchenko, Tamas Meszaros, Lukas Matena, Vojtech Kral, David Kocik and numerous others.");
+                        _L("Contributions by Melvyn ROLLAND, Leonardo and numerous others.");
 
             title_font = version_font = credits_font = init_font;
         }
